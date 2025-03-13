b70ddf7fb2ef12df045f80704dcaf3a275af0da5
hahahaha3 <eksql2689@gmail.com>
2025-03-13 09:38:39 +0900

    Updates

---
+++ assets/Button.stories-f0e1dc28.js
@@ -0,0 +1,25 @@
+import{f as z}from"./index-80a24d76.js";import{B as x}from"./Button-1bd474c1.js";import"./jsx-runtime-50395f49.js";import"./index-9fa1aa67.js";const L={title:"Example/Button",component:x,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:z()}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},s={args:{size:"small",label:"Button"}},o={args:{primary:!1,label:"Button"}};var t,n,c;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var l,m,p;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...(p=(m=a.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var u,i,g;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...(g=(i=e.parameters)==null?void 0:i.docs)==null?void 0:g.source}}};var d,B,y;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...(y=(B=s.parameters)==null?void 0:B.docs)==null?void 0:y.source}}};var b,S,f;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    primary: false,
+    label: "Button"
+  }
+}`,...(f=(S=o.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};const P=["Primary","Secondary","Large","Small","DesignSystem"];export{o as DesignSystem,e as Large,r as Primary,a as Secondary,s as Small,P as __namedExportsOrder,L as default};
